d2e635a6444d669d6a1992edf06f57a3adf46550
1Wambugu <martingakuya@gmail.com>
2023-10-01 01:00:15 +0300

    Completed List Comprehensions and Generator Expressions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+# list_comprehension.py
+
 def return_evens(num_list):
-    pass
+    """
+    Returns a list of all even elements from the input list of numbers.
+    """
+    return [x for x in num_list if x % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    """
+    Adds an exclamation mark at the end of each sentence in the input list of sentences.
+    """
+    return [sentence + '!' for sentence in sentence_list]
